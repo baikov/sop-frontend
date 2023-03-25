c65a8c862f010bb7b320bd0320d144c56ac34dbe
Alexey Baikov <baikov52@gmail.com>
2023-03-26 02:12:33 +0300

    Add contacts page

---
+++ pages/contacts/index.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Контактная информация ООО "СПЕЦОПТТОРГ"',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Контакты компании ООО "СПЕЦОПТТОРГ"',
+    },
+  ],
+})
+const breadcrumbs = [
+  {
+    level: 1,
+    name: 'Контакты',
+    href: '/contacts',
+    disabled: true,
+  },
+]
+</script>
+
+<template>
+  <PageBreadcrumbs :items="breadcrumbs" />
+  <section class="relative mx-auto flex h-full w-full flex-1 flex-col">
+    <div class="mx-auto w-full max-w-7xl p-4">
+      <h1 class="my-2 text-2xl font-bold text-gray-800 md:text-3xl">
+        Контакты
+      </h1>
+    </div>
+  </section>
+</template>

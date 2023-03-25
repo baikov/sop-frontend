f2294c60f7455d93b9890e3e6ff40b1929c4f9a0
Alexey Baikov <baikov52@gmail.com>
2023-03-26 02:10:30 +0300

    Fix index meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
 useHead({
-  title: 'Nuxt3',
-  titleTemplate: '%s + Tailwind starter template',
+  title: 'ООО "СПЕЦОПТТОРГ" - поставки стальной трубы, металлопроката, изоляции, опор для трубопроводов',
+  titleTemplate: '%s + Нижний Новгород',
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     {
       hid: 'description',
       name: 'description',
-      content: 'Good start point for your project',
+      content: 'Широкий выбор стальной трубы, металлопроката, деталей для трубопровода. Услуги доставки.',
     },
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -16,8 +16,10 @@ useHead({
 
 <template>
   <section class="relative mx-auto flex h-full w-full flex-1 flex-col">
-    <div class="mx-auto max-w-7xl">
-      <h1>Test Header</h1>
+    <div class="mx-auto w-full max-w-7xl p-4">
+      <h1 class="my-2 text-2xl font-bold text-gray-800 md:text-3xl">
+        Главная страница
+      </h1>
     </div>
   </section>
 </template>

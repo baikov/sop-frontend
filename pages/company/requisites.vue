72661d69cc155e6c21ad1c242899d52d1d98f36b
Alexey Baikov <baikov52@gmail.com>
2023-03-26 02:11:37 +0300

    Add requisites page

---
+++ pages/company/requisites.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Реквизиты ООО "СПЕЦОПТТОРГ"',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Реквизиты компании ООО "СПЕЦОПТТОРГ"',
+    },
+  ],
+})
+const breadcrumbs = [
+  {
+    level: 1,
+    name: 'О компании',
+    href: '/company/',
+    disabled: false,
+  },
+  {
+    level: 2,
+    name: 'Реквизиты',
+    href: '/company/requisites',
+    disabled: true,
+  },
+]
+</script>
+
+<template>
+  <PageBreadcrumbs :items="breadcrumbs" />
+  <section class="relative mx-auto flex h-full w-full flex-1 flex-col">
+    <div class="mx-auto w-full max-w-7xl p-4">
+      <h1 class="my-2 text-2xl font-bold text-gray-800 md:text-3xl">
+        Реквизиты
+      </h1>
+    </div>
+  </section>
+</template>

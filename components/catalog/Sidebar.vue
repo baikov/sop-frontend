222e82c56ae80cde6ed55eefe47ca51156821cdc
Alexey Baikov <baikov52@gmail.com>
2023-03-26 00:52:30 +0300

    Add Sidebar and LeftMenu components

---
+++ components/catalog/Sidebar.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { getCatalogMenu } = useMenu()
+const catalogMenu = await getCatalogMenu()
+// if (catalogMenu === null) {
+//   showError({ statusCode: 404, statusMessage: "Page Not Found" });
+// }
+</script>
+
+<template>
+  <div>
+    <CatalogLeftMenu :menu="catalogMenu" />
+    <p>After menu</p>
+  </div>
+</template>

be24371746ff74e7c0741dc95b3677a09d91618d
Alexey Baikov <baikov52@gmail.com>
2023-06-12 20:35:15 +0300

    Hotfix. hover на подкатегории каталога в светлой теме

--- components/catalog/Subcategories.vue
+++ components/catalog/Subcategories.vue
@@ -23,7 +23,7 @@ defineProps<{
                 v-show="cat.image" :src="`/soptorg/media/${cat.image}`" width="120" height="60" loading="lazy"
                 class="transition-all ease-in-out duration-300 inline-block relative top-0 group-hover:-top-0.5 dark:invert"
               />
-              <h3 class="transition-all ease-in-out duration-300 text-md group-hover:text-zinc-100 text-center mt-3">
+              <h3 class="transition-all ease-in-out duration-300 text-md dark:group-hover:text-zinc-100  text-center mt-3">
                 {{ cat.name }}
               </h3>
               <!-- <span class="transition-all ease-in-out duration-300 text-md text-gray-400 group-hover:text-gray-600">2,041</span> -->
